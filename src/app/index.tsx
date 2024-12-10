a7ad273f98fe4a7e96656eed90422d04a2f22e3c
Sara Cavalcante <saracavalcante@ioasys.com.br>
2024-12-10 10:56:20 -0300

    Add button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native"
 
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 
 export default function Index() {
     return (
@@ -9,6 +10,10 @@ export default function Index() {
             style={{ flex: 1, padding: 40, gap: 40 }}>
             <Welcome />
             <Steps />
+
+            <Button>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
