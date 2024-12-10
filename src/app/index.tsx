be863412c0e7e80be9096b97f7e641537cd2cb4a
Sara Cavalcante <saracavalcante@ioasys.com.br>
2024-12-10 09:19:50 -0300

    Add welcome component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native"
 
+import { Welcome } from "@/components/welcome"
+
 export default function Index() {
     return (
-        <View>
-            <Text>Hello React Native!</Text>
+        <View
+            style={{ flex: 1, padding: 40, gap: 40 }}>
+            <Welcome />
         </View>
     )
 }
