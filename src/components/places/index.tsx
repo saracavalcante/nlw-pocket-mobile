8f1c31836e6c008cd35f81c4743f2beec6941397
Sara Cavalcante <saracavalcante@ioasys.com.br>
2024-12-11 08:12:24 -0300

    Finish bottom sheet

--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -13,11 +13,27 @@ export function Places({ data }: Props) {
     const dimensions = useWindowDimensions()
     const bottomSheetRef = useRef<BottomSheet>(null)
 
-    return <BottomSheet>
+    const snapPoints = {
+        min: 278,
+        max: dimensions.height - 128
+    }
+
+    return <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[snapPoints.min, snapPoints.max]}
+        handleIndicatorStyle={s.indicator}
+        backgroundStyle={s.container}
+        enableOverDrag={false}
+    >
         <BottomSheetFlatList
             data={data}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => <Place data={item} />}
+            contentContainerStyle={s.content}
+            ListHeaderComponent={() => (
+                <Text style={s.title}>Explore locais perto de você</Text>
+            )}
+            showsVerticalScrollIndicator={false}
         />
     </BottomSheet>
 }
